49745c5f0fe9c562bb570e688e9f5396e89e6959
Greg Schullo <greg.schullo@gmail.com>
2019-10-29 15:57:51 -0600

    updated siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'Documentation', // Title for your website.
-  tagline: 'A website for my tech documentation and writing',
+  tagline: 'Tech Documentation and Writing',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'greg-documentation',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
